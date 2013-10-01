cbf60ff30eb9b829436f79c51d0a2a2f8e1fb45c
Carl Lipo <clipo@csulb.edu>
2013-09-30 17:50:35 -0700

    adding output code. Lots more to do

--- python/seriate.py
+++ python/seriate.py
@@ -983,6 +983,18 @@ def setupOutput(filename, pairFlag,mstFlag):
             
     return OUTFILE, OUTPAIRSFILE, OUTMSTFILE,OUTMSTDISTANCEFILE
 
+#################################################### sort by multiple keys ####################################################
+def multikeysort(items, columns):
+    from operator import itemgetter
+    comparers = [ ((itemgetter(col[1:].strip()), -1) if col.startswith('-') else (itemgetter(col.strip()), 1)) for col in columns]
+    def comparer(left, right):
+        for fn, mult in comparers:
+            result = cmp(fn(left), fn(right))
+            if result:
+                return mult * result
+        else:
+            return 0
+    return sorted(items, cmp=comparer)
 
 #################################################### OUTPUT SECTION ####################################################
 def output(assemblages,assemblageSize,distanceBetweenAssemblages,xAssemblage,yAssemblage,largestX,largestY,filteredArray,
